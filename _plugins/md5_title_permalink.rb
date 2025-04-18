3b816f9049e5991d20fee4cc527714fb66f8ce89
thuhoai27 <125796995+thuhoai27@users.noreply.github.com>
2025-04-18 13:49:12 +0900

    12

--- _plugins/md5_title_permalink.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-require "digest/md5"
-
-Jekyll::Hooks.register :documents, :pre_render do |doc|
-  next unless doc.data["title"] # 제목 없는 파일은 건너뜀
-  doc.data["md5title"] = Digest::MD5.hexdigest(doc.data["title"].to_s)
-end
